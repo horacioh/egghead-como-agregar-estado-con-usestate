e7a72bf996bf7e632c8bf4032ae32de8f16a2862
Horacio Herrera <hi@horacioh.com>
2020-09-02 17:43:20 +0200

    initial commit

---
+++ src/Settings.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Field from "./Field"
+
+function Settings() {
+  const user = {}
+  return (
+    <div className="py-12">
+      <div className="mx-auto px-8 w-full max-w-3xl">
+        <h1 className="text-gray-800 font-bold text-lg py-2 border-b">
+          Settings
+        </h1>
+        <Field label="First Name" value={user.firstName} />
+        <Field label="Last Name" value={user.lastName} />
+        <Field label="Email" value={user.email} />
+        <Field label="ID" value={user.id} />
+      </div>
+    </div>
+  )
+}
+
+export default Settings
